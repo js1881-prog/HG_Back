86118fbfa21bf559a6d088f27f50af8e56e3bc94
leekh <amysun125@gmail.com>
2023-05-17 21:34:56 +0900

    [add] Feature comment

---
+++ src/comment/commentController.js
@@ -0,0 +1,70 @@
+const commentService = require("./commentService");
+const logger = require("../util/logger/logger");
+const buildResponse = require("../util/response/buildResponse");
+
+const commentController = {
+  async createComment(req, res, next) {
+    try {
+      const { content, trip_id } = req.body;
+      const userId = req.user.id;
+      const newComment = await commentService.createComment({
+        user_id: userId,
+        content,
+        trip_id,
+      });
+      res.status(201).json(buildResponse(newComment));
+    } catch (error) {
+      logger.error(error);
+      next(error);
+    }
+  },
+
+  async getCommentById(req, res, next) {
+    try {
+      const commentId = req.params.commentId;
+      const comment = await commentService.getCommentById(commentId);
+      res.status(200).json(buildResponse(comment));
+    } catch (error) {
+      logger.error(error);
+      next(error);
+    }
+  },
+
+  async updateComment(req, res, next) {
+    try {
+      const commentId = req.params.commentId;
+      const updatedComment = req.body;
+      const comment = await commentService.updateComment(
+        commentId,
+        updatedComment
+      );
+      res.status(200).json(buildResponse(comment));
+    } catch (error) {
+      logger.error(error);
+      next(error);
+    }
+  },
+
+  async getAllComments(req, res, next) {
+    try {
+      const comments = await commentService.getAllComments();
+      res.status(200).json(buildResponse(comments));
+    } catch (error) {
+      logger.error(error);
+      next(error);
+    }
+  },
+
+  async deleteComment(req, res, next) {
+    try {
+      const commentId = req.params.commentId;
+      await commentService.deleteComment(commentId);
+      res.status(204).send();
+    } catch (error) {
+      logger.error(error);
+      next(error);
+    }
+  },
+};
+
+module.exports = commentController;
