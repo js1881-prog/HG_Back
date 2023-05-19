1ec61eefed9ec229253ac0f72c0ee285c3eb0a7d
leekh <amysun125@gmail.com>
2023-05-19 21:17:23 +0900

    [style] update table name

--- src/search/searchController.js
+++ src/search/searchController.js
@@ -1,6 +1,5 @@
 // 검색 API 엔드포인트 처리
 const searchService = require("./searchService");
-const logger = require("../util/logger/logger");
 const buildResponse = require("../util/response/buildResponse");
 
 const searchController = {
@@ -10,7 +9,6 @@ const searchController = {
       const searchResult = await searchService.search(searchText);
       res.status(200).json(buildResponse(searchResult));
     } catch (error) {
-      logger.error(error);
       next(error);
     }
   },
